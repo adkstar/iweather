2b9d9db5e2897d8fa7f05c981916e1fba6acb9b8
Abdi Daraje <adkstar10@gmail.com>
2023-05-22 15:20:44 +0300

    added fast-xml-parser and imported the provided util helper to parse XML into js object

---
+++ utils/NwsApiHelpers.js
@@ -0,0 +1,59 @@
+import {XMLParser} from 'fast-xml-parser';
+
+export function parseForecast(xmlText) {
+  const raw = parseToJsObj(xmlText);
+  const nice = extractParameters(raw);
+  return nice;
+};
+
+export function parseToJsObj(xmlText) {
+  let parseOptions = {
+    ignoreAttributes: false,
+    parseAttributeValue: true,
+    attributesGroupName: "@",
+    attributeNamePrefix: "",
+  };
+  const parser = new XMLParser(parseOptions);
+  const jsObj = parser.parse(xmlText);
+  return jsObj;
+}
+
+export function extractParameters(forecast) {
+  const startTimes = forecast.dwml.data["time-layout"]["start-valid-time"];
+  const endTimes = forecast.dwml.data["time-layout"]["end-valid-time"];
+  let output = [];
+
+  for (let index = 0; index < startTimes.length; index++) {
+    output.push({
+      startTime: new Date(startTimes[index]),
+      endTime: new Date(endTimes[index]),
+    });
+  }
+
+  for (const key in forecast.dwml.data.parameters) {
+    const values = forecast.dwml.data.parameters[key];
+    const category = key;
+
+    for (let ii = 0; ii < values.length; ii++) {
+      const item = values[ii];
+      const itemValues = item.value;
+      const itemAttributes = item["@"];
+      const metric = itemAttributes.type;
+
+      for (let jj = 0; jj < itemValues.length; jj++) {
+        const measurement = itemValues[jj];
+        if (typeof measurement === "object") {
+          if (measurement["@"]["xsi:nil"]) {
+            output[jj][metric] = null;
+          } else {
+            throw new Error("Unexpected element");
+          }
+        } else {
+          output[jj][metric] = measurement;
+        }
+      }
+    }
+  }
+
+  return output;
+}
